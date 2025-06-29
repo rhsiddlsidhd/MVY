cdb8fe8b8253802cd0aa84aa7d20e73508bb0e29
young <rhsiddlsidhd@nate.com>
2025-06-29 11:08:23 +0900

    starRate add

--- src/app/components/atoms/Card.tsx
+++ src/app/components/atoms/Card.tsx
@@ -4,17 +4,19 @@ import React, { useState } from "react";
 import Img from "./Img";
 import Badge from "./Badge";
 import OutlineText from "./OutlineText";
+import { StarRateIcon } from "./Icon";
 
 const Card = ({ className, data }: { data: MovieList; className?: string }) => {
   const [mouseEnter, setMouseEnter] = useState(false);
 
-  const { backdrop_path, adult, title, original_title } = data;
+  const { backdrop_path, adult, title, original_title, vote_average } = data;
 
   return (
-    <div
+    <a
       className={`${className} relative rounded-4xl overflow-hidden`}
       onMouseEnter={() => setMouseEnter(true)}
       onMouseLeave={() => setMouseEnter(false)}
+      href="/"
     >
       <div className="relative rounded-4xl w-full h-full brightness-30 overflow-hidden">
         <Img alt="이미지" src={backdrop_path} />
@@ -28,15 +30,21 @@ const Card = ({ className, data }: { data: MovieList; className?: string }) => {
         } duration-300 ease-in`}
       />
       <div className="absolute w-full top-[15%] left-[1rem] flex flex-col tracking-[0.15rem]">
-        <OutlineText className="text-[5vw] line-clamp-2 border-2 brightness-200">
+        <OutlineText className="text-[5vw] line-clamp-2 brightness-200">
           {original_title}
         </OutlineText>
 
         <OutlineText className="text-[3vw] line-clamp-2 brightness-200">
           ( {title} )
         </OutlineText>
+        <div className="flex items-center gap-1">
+          <StarRateIcon className="text-[1.5rem] text-[#FFD34F]" />
+          <OutlineText className="text-[1rem] brightness-200 ">
+            {vote_average.toFixed(1)}
+          </OutlineText>
+        </div>
       </div>
-    </div>
+    </a>
   );
 };
 
--- src/app/components/atoms/Icon.tsx
+++ src/app/components/atoms/Icon.tsx
@@ -3,6 +3,7 @@ import { SvgIcon } from "@mui/material";
 import Search from "@mui/icons-material/Search";
 import Close from "@mui/icons-material/Close";
 import Menu from "@mui/icons-material/Menu";
+import StarRate from "@mui/icons-material/StarRate";
 
 export interface IconButtonProps {
   onClick?: React.MouseEventHandler<SVGSVGElement>;
@@ -32,3 +33,5 @@ export const CloseIcon = withIconStyle(Close);
 export const SearchIcon = withIconStyle(Search);
 
 export const HamburgerIcon = withIconStyle(Menu);
+
+export const StarRateIcon = withIconStyle(StarRate);
